8151216d057b80f705cc0b60c2e5ff6e3fd7b02b
Dmitry <majest228@gmail.com>
2024-03-12 23:10:42 +0500

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 // export const URL = `http://localhost:8080/api-v2`;
-export const URL = `http://194.169.160.152:8081/api-v2/`;
+export const URL = `http://194.169.160.152:8081/api-v2`;
 
 export const $apiWithToken = axios.create({
   withCredentials: true,
